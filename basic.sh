1111281ab6e6ed84b69705c1d83e7d936d84566a
ulfandpete <mail@ulfpetersen.com>
2013-10-25 18:21:13 +0200

    Added other .sh files as option

--- basic.sh
+++ basic.sh
@@ -15,3 +15,29 @@ sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
+# Ask whether to continue with the next setup-files or not
+read -p "Basic setup complete. Do you want to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]|[Yy]es$ ]]
+then
+    curl https://raw.github.com/ulfandpete/setup/master/setup.sh | bash
+    
+    read -p "Install startup-class files? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]|[Yy]es$ ]]
+    then
+      curl https://raw.github.com/ulfandpete/setup/master/startup-class.sh | bash
+    fi
+    read -p "Install derby? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]|[Yy]es$ ]]
+    then
+      curl https://raw.github.com/ulfandpete/setup/master/derby.sh | bash
+    fi
+    read -p "Install meteor? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]|[Yy]es$ ]]
+    then
+      curl https://raw.github.com/ulfandpete/setup/master/meteor.sh | bash
+    fi
+fi
