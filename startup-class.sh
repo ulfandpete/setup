60b9bf3c6aa24922ebd8e3ce6fbb27c8b15f15f9
ulfandpete <mail@ulfpetersen.com>
2013-10-26 13:14:35 +0200

    Delete startup-class.sh

--- startup-class.sh
+++
@@ -1,6 +0,0 @@
-!/bin/bash
-# Installs a basic bitcoin app based on nodejs and the mean-stack without mongoDB
-# for a headless setup (whatever this means)
-
-curl https://raw.github.com/startup-class/setup/master/setup.sh | bash
-sudo shutdown -r now
