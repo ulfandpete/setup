004f172439c92cdf67a6d9e00ac460f6a3a71e3f
ulfandpete <mail@ulfpetersen.com>
2013-10-27 14:21:34 +0100

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -37,5 +37,6 @@ then
   wget https://raw.github.com/ulfandpete/setup/master/setup.sh
   sudo chmod 777 setup.sh
   read -p "Press a button to restart the VM. You can execute setup.sh to continue."
+  rm basic.sh
   sudo shutdown -r now
 fi
