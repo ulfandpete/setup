281c75b55d52babc94849229ff264cf32539dbd6
ulfandpete <mail@ulfpetersen.com>
2013-11-16 20:50:47 +0100

    Update derby.sh

--- derby.sh
+++ derby.sh
@@ -2,16 +2,16 @@
 # Install all the files necessary for derby.js
 
 # Install Redis
-wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz
-tar -xzf redis-2.6.14.tar.gz
-cd redis-2.6.14
-sudo make
+# wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz
+# tar -xzf redis-2.6.14.tar.gz
+# cd redis-2.6.14
+# sudo make
 
 # Install mongoDB
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-10gen redis-server
 
 # Finally install derby
 sudo npm install -g derby
