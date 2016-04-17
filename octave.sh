640762709623d73f8b7cb3332a65af280b01f5a8
ulfandpete <mail@ulfpetersen.com>
2016-04-17 16:37:07 +0200

    Update octave.sh

--- octave.sh
+++ octave.sh
@@ -1,2 +1,3 @@
-# These lines help to get octave up and running on the ubuntu machine
+#!/bin/bash
+# This file helps to get octave up and running on the ubuntu machine
 sudo apt-get update && sudo apt-get install -y octave
